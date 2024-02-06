5580a313fe239680939b50672273b637489a0ccb
Kuchteq <mariusz.kuchta@triona.fi>
2024-02-06 19:35:12 +0100

    Add sb internet/improve syntax

--- blocks.h
+++ blocks.h
@@ -1,13 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/*Icon*/   /*Command*/	 /*Update Interval*/	/*Update Signal*/
 	{"",	"sb-screenshot -d",	0,	13},
 	{"",	"sb-cpu",		3,	18}, 
-	{"",	"sb-memory",	3,	14}, 
-	{"",	"sb-battery",	10,	3},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-lock",	0,	11},
-	{"",	"sb-recording",	0,	12},
+	{"",	"sb-memory",            3,	14}, 
+	{"",	"sb-battery",           10,	3},
+	{"",	"sb-volume",            0,	10},
+	{"",	"sb-lock",              0,	11},
+	{"",	"sb-recording    ",     0,	12},
+	{"",	"sb-internet",          3,	14}, 
 	{"", "date '+%a %b %d %I:%M %p'",					5,		0},
 };
 
