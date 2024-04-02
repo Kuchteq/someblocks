1a60b64a06bf66576d894fc4a54fe471ad75e721
Kuchteq <mariusz.kuchta@triona.fi>
2024-04-02 14:21:13 +0200

    Change sb-screenshot to sb-notifier to generalize notifications on bar

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/   /*Command*/	 /*Update Interval*/	/*Update Signal*/
-	{"",	"sb-screenshot -d",	0,	13},
+	{"",	"sb-notifier -d",	0,	13},
 	{"",	"sb-cpu",		3,	18}, 
 	{"",	"sb-memory",            3,	14}, 
 	{"",	"sb-battery",           10,	3},
